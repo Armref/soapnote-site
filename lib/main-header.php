23b26c622c54f75ab94bc3dea09b973dcca63043
soapnote <mark@soapnote.org>
2016-09-25 08:20:24 -0500

    back to original analytics

--- lib/main-header.php
+++ lib/main-header.php
@@ -16,22 +16,14 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script async src="//www.soapnote.org/lib/list-filter.js"></script>
-	<script async src="//www.soapnote.org/lib/autotrack.js"></script>
 	<script>
-		window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
 		ga('create', 'UA-15622745-1', 'auto');
-
-		// Replace the following lines with the plugins you want to use.
-		ga('require', 'eventTracker');
-		ga('require', 'outboundLinkTracker');
-		ga('require', 'outboundFormTracker');
-		ga('require', 'urlChangeTracker');
-		// ...
-
 		ga('send', 'pageview');
-		</script>
-		<script async src='https://www.google-analytics.com/analytics.js'></script>
-		<script async src='path/to/autotrack.js'>
 	</script>
 </head>
 <body style="padding-bottom:40px">
